66c9e27a7b24509fb567f9378be76c1767b72e15
Iury Cardoso <iurycardoso6@gmail.com>
2019-10-27 11:08:11 -0300

    Configuracao de Rotas

--- cartorio-de-protestos/src/app/app-routing.module.ts
+++ cartorio-de-protestos/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ServicosComponent } from './servicos/servicos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CartoriosComponent } from './cartorios/cartorios.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'cartorios', component: CartoriosComponent },
+  { path: 'servicos', component: ServicosComponent },
+  { path: 'calculadora', component: CalculadoraComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
