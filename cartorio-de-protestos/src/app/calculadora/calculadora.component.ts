98078c2c002535118477dff9436fcd34cd892a3e
Iury Cardoso <iurycardoso6@gmail.com>
2019-10-28 10:41:37 -0300

    Finalizacao Calculadora

--- cartorio-de-protestos/src/app/calculadora/calculadora.component.ts
+++ cartorio-de-protestos/src/app/calculadora/calculadora.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
+
+
+
 export class CalculadoraComponent implements OnInit {
+
+  public numero1: number;
+	public numero2: number;
+	public resultato: number;
+	public resumo: any;
+
+	adicao() {
+		this.resultato = this.numero1 + this.numero2;
+		this.resumo = this.numero1 + ' + ' + this.numero2;
+	}
+
+	subtracao() {
+		// tslint:disable-next-line: indent
+		this.resultato = this.numero1 - this.numero2;
+		this.resumo = this.numero1 + ' - ' + this.numero2;
+	}
+	multiplicacao() {
+		this.resultato = this.numero1 * this.numero2;
+		this.resumo = this.numero1 + ' x ' + this.numero2;
+	}
+	divisao() {
+			this.resultato = this.numero1 / this.numero2;
+			this.resumo = this.numero1 + ' / ' + this.numero2;
+		}
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  
 
 }
